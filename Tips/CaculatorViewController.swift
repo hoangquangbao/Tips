1fd72d3ffa9d737894249acdd8a1769b38f92d95
hoangquangbao <hoangquangbao.developer@gmail.com>
2020-09-02 23:57:31 +0700

    Create IB

---
+++ Tips/CaculatorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Tips
+//
+//  Created by Boboli on 8/28/20.
+//  Copyright © 2020 Boboli. All rights reserved.
+//
+
+import UIKit
+
+class CaculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var tenButton: UIButton!
+    @IBOutlet weak var twentyButton: UIButton!
+    @IBOutlet weak var splitsNumberLable: UILabel!
+    
+    
+    @IBAction func tipsChanged(_ sender: UIButton) {
+    }
+    @IBAction func splitChanged(_ sender: UIStepper) {
+    }
+    @IBAction func caculatorPressed(_ sender: UIButton) {
+    }
+    
+}
+
